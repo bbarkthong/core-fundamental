8dc29af03dd07f8ef870106d363c85525224ef66
bbarkthong <bbarkthong@gmail.com>
2022-11-08 21:35:38 +0900

    first commit

---
+++ src/main/java/com/basah/spring/corefundamental/CoreFundamentalApplication.java
@@ -0,0 +1,13 @@
+package com.basah.spring.corefundamental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreFundamentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreFundamentalApplication.class, args);
+	}
+
+}
