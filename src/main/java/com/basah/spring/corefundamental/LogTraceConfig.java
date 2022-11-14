407cb914df5229caf29f3072d95eae5a4580db50
bbarkthong <bbarkthong@gmail.com>
2022-11-14 23:04:09 +0900

    Thread Local 반영

--- src/main/java/com/basah/spring/corefundamental/LogTraceConfig.java
+++ src/main/java/com/basah/spring/corefundamental/LogTraceConfig.java
@@ -3,14 +3,14 @@ package com.basah.spring.corefundamental;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.basah.spring.corefundamental.trace.logtrace.FieldLogTrace;
 import com.basah.spring.corefundamental.trace.logtrace.LogTrace;
+import com.basah.spring.corefundamental.trace.logtrace.ThreadLocalLogTrace;
 
 @Configuration
 public class LogTraceConfig {
 
     @Bean
     public LogTrace logTrace() {
-        return new FieldLogTrace();
+        return new ThreadLocalLogTrace();
     }
 }
